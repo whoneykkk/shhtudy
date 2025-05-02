97fae3bdb685a6402bdfaf2d1bf30dda9dccb63c
whoneykkk <whoneykim@gmail.com>
2025-05-02 16:01:57 +0900

    feat(notice): 공지사항 목록 조회 API 구현 및 읽음 여부 처리 로직 추가
    - 사용자 인증(Firebase ID 토큰) 기반 공지 조회 기능 구현
    - 사용자가 읽은 공지사항은 isRead: true로 응답
    - NoticeController, NoticeService, NoticeResponseDto 작성 완료

---
+++ backend/src/main/java/com/shhtudy/backend/service/NoticeService.java
@@ -0,0 +1,48 @@
+package com.shhtudy.backend.service;
+
+import com.shhtudy.backend.dto.NoticeResponseDto;
+import com.shhtudy.backend.entity.Notice;
+import com.shhtudy.backend.entity.NoticeRead;
+import com.shhtudy.backend.entity.User;
+import com.shhtudy.backend.exception.CustomException;
+import com.shhtudy.backend.exception.code.ErrorCode;
+import com.shhtudy.backend.repository.NoticeReadRepository;
+import com.shhtudy.backend.repository.NoticeRepository;
+import com.shhtudy.backend.repository.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeService {
+    private final NoticeRepository noticeRepository;
+    private final NoticeReadRepository noticeReadRepository;
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly=true)
+    public List<NoticeResponseDto> getNoticeWithRaeadStatus(String userId) {
+        User user = userRepository.findById(userId).
+                orElseThrow(()-> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        List<Notice> notices = noticeRepository.findAllByOrderByCreatedAtDesc();
+
+        List<NoticeRead> reads = noticeReadRepository.findAllByUser(user);
+        //사용자가 읽은 공지 ID 목록
+        Set<Long> readNoticeIds = reads.stream()
+                .map(nr -> nr.getNotice().getId())
+                .collect(Collectors.toSet());
+
+        //공지사항 리스트를 dto로 변환하면서 읽음 여부 판단
+        return notices.stream()
+                .map(notice -> new NoticeResponseDto(
+                        notice,
+                        readNoticeIds.contains(notice.getId())
+                ))
+                .toList();
+    }
+}
