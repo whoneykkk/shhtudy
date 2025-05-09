c48d15cc6723269f65b2f24920d49ba3f73279a5
whoneykkk <whoneykim@gmail.com>
2025-05-09 22:42:35 +0900

    feat(message): 메시지 목록 API 리팩토링 및 페이지네이션 적용

---
+++ backend/src/main/java/com/shhtudy/backend/repository/SeatRepository.java
@@ -0,0 +1,7 @@
+package com.shhtudy.backend.repository;
+
+import com.shhtudy.backend.entity.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+}
