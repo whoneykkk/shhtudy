09d13afe69ad2cb6bd7408c3ca76558e1b8ea354
whoneykkk <whoneykim@gmail.com>
2025-06-11 16:32:01 +0900

    feat: Swagger 문서에 인증 설정 및 API 정보(OpenAPI) 추가

--- backend/src/main/java/com/shhtudy/backend/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/shhtudy/backend/global/config/SwaggerConfig.java
@@ -2,13 +2,33 @@ package com.shhtudy.backend.global.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 @SecurityScheme(
         name = "FirebaseToken",
         type = SecuritySchemeType.HTTP,
         scheme = "bearer",
-        bearerFormat = "JWT"
+        bearerFormat = "JWT",
+        description = "Firebase Authentication에서 발급받은 ID 토큰을 입력하세요."
 )
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("쉿터디(Shh-tudy) API")
+                .description("종합 설계 프로젝트 API 문서입니다.")
+                .version("1.0");
+
+        SecurityRequirement firebaseSecurity = new SecurityRequirement().addList("FirebaseToken");
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(firebaseSecurity);
+    }
 }
